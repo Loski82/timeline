f1ae7a656508fb44238c07cc8239e1a4d4f66a4d
loski82 <carlos.ramia@careerbuilder.com>
2015-12-09 17:42:22 +0000

    added boostrap and user id and project id to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209172046) do
+ActiveRecord::Schema.define(version: 20151209174125) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20151209172046) do
     t.datetime "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "project_id"
   end
 
   create_table "users", force: :cascade do |t|
